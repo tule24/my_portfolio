480bc917770579cfb100656191c8da36c6fc52e7
lecongtu <tulecong24@gmail.com>
2023-03-20 14:20:38 +0700

    update

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
         <h1 className='ribbon'>About Me</h1>
       </div>
       <div className='px-5 pt-5 text-justify text-slate-900 dark:text-[rgb(166,166,166)] tracking-wider'>
-        <p>My name is Cong Tu. I live in Ho Chi Minh City. I'm currently pursuing a career as a Web3 developer. With 2 years of experience working for a technology start-up, I believe that my knowledge and experience will will be very valuable to the business. I'm a progressive man, I have the ability to self-study and research new technology. I'm hoping to land a job that will allow me to reach my full potential.</p>
+        <p>My name is Cong Tu. I live in Ho Chi Minh City. I'm currently pursuing a career as a Web developer. With 2 years of experience working for a technology start-up, I believe that my knowledge and experience will will be very valuable to the business. I'm a progressive man, I have the ability to self-study and research new technology. I'm hoping to land a job that will allow me to reach my full potential.</p>
       </div>
     </div>
   )
